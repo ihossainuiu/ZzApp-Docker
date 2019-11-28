190901efe6987d88beb26526a11513655d36f179
ihossain <ihossain.mac@gmail.com>
2019-11-28 19:00:38 +0600

    Item Controller

--- zzapp-docker/src/main/java/com/example/controller/ItemController.java
+++ zzapp-docker/src/main/java/com/example/controller/ItemController.java
@@ -1,11 +1,13 @@
 package com.example.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,16 +17,31 @@ import com.example.entity.Item;
 import com.example.service.ItemService;
 
 @RestController
-@RequestMapping(path = "/book")
+@RequestMapping(path = "/")
 @ResponseBody
 public class ItemController {
 
 	@Autowired
 	private ItemService itemService;
 
-	@GetMapping("/")
+	@GetMapping("items")
 	public List<Item> items() {
 		return itemService.findAll();
+	}
+
+	@GetMapping("items/{id}")
+	public String retrieveItem(@PathVariable long id) {
+		return itemService.getItem(id);
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello Zzapp";
+	}
+
+	@GetMapping("current_time")
+	public String current_time() {
+		return new Date().toString();
 	}
 
 	@Autowired
